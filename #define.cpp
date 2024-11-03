53bd1d86a92d4c5dbbe62c858cf2a34c4972ad55
Katherine-0224 <siyuxie0224@gmail.com>
2024-11-03 18:58:23 +0800

    Add files via upload

---
+++ #define.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+#define PI 3.14
+
+int main(){
+    cout << "The value of PI is: " << PI << endl;
+    return 0;
+}
+
+// #include <iostream>
+// int main(){
+//     cout << "The value of PI is: " << 3.14 << endl;
+//     return 0;
+// }
